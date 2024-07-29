3c4fde5db1e5856cef20b7b8f310b2118367639f
mrfarhankhan007 <fk95034@gmail.com>
2024-07-29 07:13:03 +0530

    data fetching in React usign nodejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
-import React from 'react';
+import { useEffect, useRef, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
+const BASE_URL = "https://jsonplaceholder.typicode.com";
+
+interface Post {
+  id: number;
+  title: string;
+}
+
 function App() {
+
+  const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [page, setPage] = useState(0);
+
+  const abortControllerRef = useRef<AbortController | null>(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      abortControllerRef.current?.abort();
+      abortControllerRef.current = new AbortController();
+
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(`${BASE_URL}/posts?page=${page}`, {
+          signal: abortControllerRef.current?.signal,
+        });
+        const posts = (await response.json()) as Post[];
+        setPosts(posts);
+      } catch (e: any) {
+        if (e.name === "AbortError") {
+          console.log("Aborted");
+          return;
+        }
+
+        setError(e);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchPosts();
+  }, [page]);
+
+  if (error) {
+    return <div>Something went wrong! Please try again.</div>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="tutorial">
+      <h1 className="mb-4 text-2xl">Data Fething in React</h1>
+      <button onClick={() => setPage(page + 1)}>Increase Page ({page})</button>
+      {isLoading && <div>Loading...</div>}
+      {!isLoading && (
+        <ul>
+          {posts.map((post) => {
+            return <li key={post.id}>{post.title}</li>;
+          })}
+        </ul>
+      )}
     </div>
   );
 }
